e48d0915f61587c41062d634c1634555a4c28e1a
Kristen Ruprecht <ksruprec@gmail.com>
2022-03-12 23:39:13 -0500

    figuring out add to cart issue

--- README.md
+++ README.md
@@ -1,25 +1,6 @@
 # Assessment 3: HTML/CSS/JS and SQL
 - **E-commerce Business**
 
-## Important Grading Information
-- Make sure you read the [Assessment-3 Grading Rubric](https://docs.google.com/spreadsheets/d/1-YjVU8Wt7qgW8yOImASqB2uYiLBu93dVJuLYjUlEIgk/edit?usp=sharing).
-  - Database (60%)...
-    - Schema (30%)
-    - Seed Data (10%)
-    - Queries (20%)
-  - Website (40%)...
-    - Structure (20%)
-    - Look & Layout (10%)
-    - Functionality (10%)
-- You need to get a 75% or better to pass. (You must pass all assessments in order to graduate Code Platoon's program.)
-- If you fail the assessment, you have can retake it once to improve your score. For this assessment... 
-  - *5% penalty*: If you complete and submit the retake **within one week** of receiving your grade.
-  - *10% penalty*: If you complete and submit the retake **by 8:30am, November 29th, 2021** (end of Thanksgiving week break). A retake for this assessment WILL NOT be accepted after this date.
-
-## Rules / Process
-- This test is fully open notes and open Google, but is not to be completed with the help of any other student/individual.
-- Do not open a pull request against this repository. We will evaluate your code individually with you.
-
 ## Requirements
 - This assessment must be completed using raw SQL on your back-end.
 - You may use either SQLite3 or PostgreSQL for your database.
--- website/script.js
+++ website/script.js
@@ -1,26 +1,37 @@
-//could not figure out how to link all html pages properly so products added to cart from any page
-//  ends up in same local storage array
+//want to add items to local storage and retrieve them in shopping cart
+//retrieve the items currently in local storage, update the data, then write back to local storage so each page doesn't rewrite local storage
+
+//DOM variables
+const btns = document.getElementsByClassName("add-to-cart");
+
 const productsKey = "products"
 const productsData = []
+
 window.onload = () => {
-    let btns = document.getElementsByClassName("add-to-cart");
-    //adding event listener only to button that was clicked
-    for (var i = 0; i < btns.length; i++) {
-        btns[i].addEventListener("click", function(e) {
-            //getting value attribute from event target that has description of product that was clicked
-            let product = e.target.getAttribute("value");
-            console.log(product)
-            //attempted to add product to unordered list
-            let listProducts = document.getElementById("cart-list")
-            if (listProducts) {
-                let newProduct = document.createElement("li");
-                newProduct.innerHTML = product;
-                listProducts.appendChild(newProduct);
-            }
-            //saving product to productsData array
-            productsData.push(product)
-            localStorage.setItem(productsKey, JSON.stringify(productsData))
-        
-        });
-    }
+    renderCart()
+}
+
+btns.addEventListener("click", function(evt) {
+    evt.preventDefault()
+    let product = evt.target.getAttribute("value")
+})
+
+//adding event listener only to button that was clicked
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function(e) {
+        //getting value attribute from event target that has description of product that was clicked
+        let product = e.target.getAttribute("value");
+        console.log(product)
+        //attempted to add product to unordered list
+        let listProducts = document.getElementById("cart-list")
+        if (listProducts) {
+            let newProduct = document.createElement("li");
+            newProduct.innerHTML = product;
+            listProducts.appendChild(newProduct);
+        }
+        //saving product to productsData array
+        productsData.push(product)
+        localStorage.setItem(productsKey, JSON.stringify(productsData))
+    
+    });
 }
